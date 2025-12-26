ea584c64f88fe3c61945f479327475e13993f469
WrightJayN <jay.n.wright06@gmail.com>
2025-12-26 12:24:08 -0500

    Add buildings to the necessary

--- Helper/templates/pathDrawer_template.js
+++ Helper/templates/pathDrawer_template.js
@@ -2,3 +2,21 @@
 ['building1', [
     // TODO: Add vertices {x: , y: }
 ]],
+['building2', [
+    // TODO: Add vertices {x: , y: }
+]],
+['building8', [
+    // TODO: Add vertices {x: , y: }
+]],
+['building22', [
+    // TODO: Add vertices {x: , y: }
+]],
+['building5', [
+    // TODO: Add vertices {x: , y: }
+]],
+['building47', [
+    // TODO: Add vertices {x: , y: }
+]],
+['building4', [
+    // TODO: Add vertices {x: , y: }
+]],
