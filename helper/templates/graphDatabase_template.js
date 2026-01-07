e50df656b4feb94a32fd5a578ac58d39070e5169
WrightJayN <jay.n.wright06@gmail.com>
2026-01-06 22:13:00 -0500

    Toggle ignorecase to false

---
+++ helper/templates/graphDatabase_template.js
@@ -0,0 +1,11 @@
+// TODO: Add graph nodes with coordinates for each building
+// Example:
+// const building1_node = new GraphNode('building1', 'building', x, y);
+// const building2_node = new GraphNode('building2', 'building', x, y);
+// const building4_node = new GraphNode('building4', 'building', x, y);
+// const building5_node = new GraphNode('building5', 'building', x, y);
+// const building8_node = new GraphNode('building8', 'building', x, y);
+// const ltbsdbld8_node = new GraphNode('ltbsdbld8', 'building', x, y);
+// const building22_node = new GraphNode('building22', 'building', x, y);
+// const building47_node = new GraphNode('building47', 'building', x, y);
+// const ltbsdbld47_node = new GraphNode('ltbsdbld47', 'building', x, y);
