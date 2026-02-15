7bff57b227f61da581561394b9b837249b447c44
WrightJayN <jay.n.wright06@gmail.com>
2026-02-14 20:14:15 -0500

    Convert jpg images to webp images

--- js/output/getBuildingPicture.js
+++ js/output/getBuildingPicture.js
@@ -2,18 +2,18 @@ class GetBuildingPicture {
 
     static getBuildingPicture(node) {
         const buildingPictures = {
-            'Faculty of Engineering and Computing': 'assets/buildings/FENC.jpg',
-            'School of Computing and Information Technology': 'assets/buildings/SCIT.jpg',
-            'building8': 'assets/buildings/building8.jpg',
-            'building22': 'assets/buildings/building22.jpg',
-            'building5': 'assets/buildings/building5.jpg',
-            'building47': 'assets/buildings/building47.jpg',
-            'building4': 'assets/buildings/building4.jpg',
-            // 'ltbsdbld8': 'assets/buildings/ltbsdbld8.jpg',
-            'ltbsdbld47': 'assets/buildings/ltbsdbld47.jpg',
-            'walkin gate': 'assets/buildings/walkin_gate.jpg',
-            'back gate': 'assets/buildings/back_gate.jpg',
-            'main gate': 'assets/buildings/main_gate.jpg',
+            'Faculty of Engineering and Computing': 'assets/buildings/FENC.webp',
+            'School of Computing and Information Technology': 'assets/buildings/SCIT.webp',
+            'building8': 'assets/buildings/building8.webp',
+            'building22': 'assets/buildings/building22.webp',
+            'building5': 'assets/buildings/building5.webp',
+            'building47': 'assets/buildings/building47.webp',
+            'building4': 'assets/buildings/building4.webp',
+            // 'ltbsdbld8': 'assets/buildings/ltbsdbld8.webp',
+            'ltbsdbld47': 'assets/buildings/ltbsdbld47.webp',
+            'walkin gate': 'assets/buildings/walkin_gate.webp',
+            'back gate': 'assets/buildings/back_gate.webp',
+            'main gate': 'assets/buildings/main_gate.webp',
         };
         const buildingName = node.name;
         return buildingPictures[buildingName];
